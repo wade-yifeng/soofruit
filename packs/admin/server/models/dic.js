3e011ce82b6fd16d40ba68cab6724cdec61d30ca
XiaozhongLiu <leo.liu@0575s.com>
2016-03-17 18:24:54 +0800

    Designed some key models(Schemas).

---
+++ packs/admin/server/models/dic.js
@@ -0,0 +1,25 @@
+/**
+ * Created by xz_liu on 2016/3/17.
+ */
+var mongoose = require('mongoose');
+require('../../../shared/server/mongoconn');
+
+/**
+ * 数据字典
+ */
+var DicSchema = new mongoose.Schema({
+
+    //类型
+    Type: String,
+
+    //名称
+    Name: String,
+
+    //数值
+    Value: Number,
+
+    //描述
+    Desc: String
+});
+
+module.exports.Order = mongoose.model('Dic', DicSchema);
---
+++ packs/mobile/server/models/good.js
@@ -0,0 +1,40 @@
+/**
+ * Created by xz_liu on 2016/3/17.
+ */
+var mongoose = require('mongoose');
+require('../../../shared/server/mongoconn');
+
+/**
+ * 商品
+ */
+var GoodSchema = new mongoose.Schema({
+
+    //名称/标题
+    name: String,
+
+    //描述
+    desc: String,
+
+    //分类
+    category: String,
+
+    //图片相对路径
+    pics: [String],
+
+    //卖价
+    sellPrice: Number,
+
+    //原价
+    originPrice: Number,
+
+    //原产地
+    provenance: String,
+
+    //保质期
+    shelfLife: Number,
+
+    //存储方式
+    storage: String
+});
+
+module.exports.Order = mongoose.model('Good', GoodSchema);
