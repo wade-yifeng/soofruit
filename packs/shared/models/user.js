35f4c7cf157e899bb984d5be734d8898cd57a265
Qin Jesse <Qin Jesse>
2016-03-27 20:41:55 +0800

    Add user model

--- packs/shared/models/user.js
+++ packs/shared/models/user.js
@@ -3,16 +3,42 @@
  */
 var mongoose = require('mongoose');
 
-/**
- * 用户
- */
 var UserSchema = new mongoose.Schema({
 
-    //用户名
-    name: String,
+    name: { type: String, unique: true },
 
-    //创建/同步日期
-    createDate:　Date
+    appUserId: String,
+
+    mobile: { type: String, unique: true },
+
+    email: { type: String, unique: true },
+
+    password: String,
+
+    description: String,
+
+    role: [String],
+
+    enabled: { type: Boolean, default: false},
+
+    weChatId: { type: String, unique: true },
+
+    //微信
+    uuid: { type: String, unique: true },
+
+    isDeleted: { type: Boolean, default: false},
+
+    lastLoginTime: Date,
+
+    createTime: { type: Date, default: Date.now},
+
+    activeTime: Date
 });
+
+UserSchema.index({ name: 1 });
+
+UserSchema.index({ uuid: 1 });
+
+UserSchema.index({ weChatId: 1 });
 
 mongoose.model('User', UserSchema);
