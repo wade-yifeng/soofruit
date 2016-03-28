9f130efec9492179d8ded0131f1cc306ad903360
XiaozhongLiu <leo.liu@0575s.com>
2016-03-28 18:04:36 +0800

    激活nav菜单; 封装公用弹框组件; 完成页面验证过程.

--- static/scripts/admin/admin.js
+++ static/scripts/admin/admin.js
@@ -1,4 +1,22 @@
-var showDialog = function (info) {
+var showInfo = function (info) {
     $('#dialogInfo .modal-body p').text(info);
     $('#dialogInfo').modal('show');
 };
+
+var showConfirm = function (info) {
+    $('#dialogConfirm .modal-body p').text(info);
+    $('#dialogConfirm').modal('show');
+};
+
+var showValidationResult = function (msgs) {
+    $('div.alert').empty();
+    for (var i in msgs) {
+        $('div.alert').append(msgs[i].msg);
+        if (msgs.length > 1 && i < msgs.length - 1) $('div.alert').append('<br/>');
+    }
+    $('div.alert').show();
+};
+
+$('.sidebar-nav li').click(function () {
+    $(this).addClass('active').siblings().removeClass('active');
+});
