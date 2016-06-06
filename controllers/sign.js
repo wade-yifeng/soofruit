e3f6a68349cee1fb4c607cbf10ccc46f4320fb46
wade-yifeng <wade.shen@0575s.com>
2016-06-06 09:16:49 +0800

    Route跳转测试1

--- controllers/sign.js
+++ controllers/sign.js
@@ -10,50 +10,54 @@ var ErrorMsg = require('../models').Enums.ErrorMessage;
 exports.login = function (req, res) {
     req.session._loginReferer = req.headers.referer;
   
-    if(config.debug) {
-        res.redirect(req.session._loginReferer);
-    }
+    // if(config.debug) {
+    //     return res.redirect(req.session._loginReferer);
+    // }
 
-    // 第一次跳转
-    if (req.query.code === undefined) {
+    if(req.query.code === undefined) { 
+        // 从微信验证跳转，但没有获取到Code
+        if(require('url').parse(req.headers.referer).hostname === "open.weixin.qq.com") {
+            res.status(403);
+            return res.render('layout.html', { error: '微信登陆失败' });
+        }
+
         var absoluteURL = req.protocol + '://' + req.get('host') + req.originalUrl;
         var url = oauthApi.getAuthorizeURL(absoluteURL);
-        res.redirect(url);
+        return res.redirect(url);
     }
-    else {
-        async.waterfall([
-            function (callback) {
-                oauthApi.getAccessToken(req.query.code, function (err, result) {
-                    if (!err && (result.data === undefined || result.data.openid === undefined)) {
-                        callback(new Error('调用微信API获取openid失败'));
-                    }
 
-                    callback(err, result.data.openid);
-                });
-            }, function (openID, callback) {
-                api.getUser(openID, function (err, result) {
-                    callback(err, result);
-                });
+    async.waterfall([
+        function (callback) {
+            oauthApi.getAccessToken(req.query.code, function (err, result) {
+                if (!err && (result.data === undefined || result.data.openid === undefined)) {
+                    callback(new Error('调用微信API获取openid失败'));
+                }
+
+                callback(err, result.data.openid);
+            });
+        }, function (openID, callback) {
+            api.getUser(openID, function (err, result) {
+                callback(err, result);
+            });
+        }
+    ], function (err, baseInfo) {
+            if (err) {
+                logger.error("获取微信身份信息失败，错误：" + err);
             }
-        ], function (err, baseInfo) {
-                if (err) {
-                    logger.error("获取微信身份信息失败，错误：" + err);
-                }
-                else {
-                    User.updateUserByUnionID(baseInfo, function(err, result) {
-                        if(err) {
-                            logger.error('更新用户失败' + err);
-                        } else{
-                            logger.info('更新或新建用户：');
-                            console.log("result: %j", result);
-                            logger.info("result: %j", result);
-                        }
-                    });
+            else {
+                User.updateUserByUnionID(baseInfo, function(err, result) {
+                    if(err) {
+                        logger.error('更新用户失败' + err);
+                    } else{
+                        logger.info('更新或新建用户：');
+                        console.log("result: %j", result);
+                        logger.info("result: %j", result);
+                    }
+                });
 
-                    auth.gen_session(baseInfo, res);
-                }
+                auth.gen_session(baseInfo, res);
+            }
 
-                res.redirect(req.session.targetUrl);
-        });
-    }
+            res.redirect(req.session.targetUrl);
+    });
 };
