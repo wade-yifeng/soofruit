f860f1e22787b0f1c716c584d426fff8a15d3e05
shen_yf <shen_yf@Ctrip.com>
2016-06-14 15:28:54 +0800

    签入service文件

---
+++ app/services/index_service.js
@@ -0,0 +1,11 @@
+var IndexService = function ($http, Promise, HttpGet) {
+    return {
+        LoadSubjectsAndExpress: function (pageIndex) {
+            return Promise(function (defer) {
+                HttpGet('index', defer);
+            });
+        }
+    };
+};
+
+IndexService.$inject = ['$http', 'Promise', 'HttpGet'];
---
+++ app/services/shared.js
@@ -0,0 +1,21 @@
+var PromiseService = function ($q) {
+    return function (logic) {
+        var defer = $q.defer();
+        logic(defer);
+        return defer.promise;
+    };
+};
+
+var HttpGetService = function ($http) {
+    return function (url, defer) {
+        $http.get(url).success(function(result) {
+            defer.resolve(result);
+        }).error(function(result, status) {
+            defer.reject(status);
+        });
+    };
+};
+
+PromiseService.$inject = ['$q'];
+
+HttpGetService.$inject = ['$http'];
