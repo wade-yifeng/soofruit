d20bd248e08acc7fc8694f15878c3f4053d4d685
shen_yf <shen_yf@Ctrip.com>
2016-06-16 19:06:32 +0800

    底部菜单

---
+++ static/scripts/app/site.js
@@ -0,0 +1,21 @@
+// 基础类库，部分基于jQuery
+(function ($, window, undefined) {
+    var Soofruit = window.Soofruit = window.Soofruit || {};
+    Soofruit.App = Soofruit.App || {};
+
+    Soofruit.App.activeFooterMenu = function(name) {
+        var currentItem = $(".footer #" + name);
+        
+        if(currentItem) {
+            currentItem.siblings().removeClass('active');
+            currentItem.addClass('active');
+        }
+    };
+
+
+}($, window, undefined));
+
+// 扩展jQuery
+(function($) {
+    
+})(jQuery);
