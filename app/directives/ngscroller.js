a8abbe04dacf044b4370d77aabc762e9f21676b0
wade-yifeng <wade.shen@0575s.com>
2016-06-21 12:18:52 +0800

    增加自定义指令

---
+++ app/directives/ngscroller.js
@@ -0,0 +1,10 @@
+/**
+ * 封装Scroller组件
+ */
+var ngScrollerDirective = function() {
+    return {
+        restrict: 'AE',
+        templateUrl: '/views/template/ngScroller',
+        replace: true
+    };
+};
